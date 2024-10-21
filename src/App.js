d046bdb25d3359d88fbabe72ead21f0d55d42061
yakhontov <yakhontov@gmail.com>
2024-10-21 20:53:24 +0300

    Some dumb try

--- src/App.js
+++ src/App.js
@@ -7,75 +7,20 @@ import Placeholder from "react-bootstrap/Placeholder"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
-async function f() {
-    // Ошибка подключения из-за CORS
-    let headersList = {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-
-    let bodyContent = "loadtable=1&sim=21_81&limit=10"
-
-    let response = await fetch("http://3.95.124.47/twiliosim/gprsconsole_test.class.php", {
-        method: "POST",
-        body: bodyContent,
-        headers: headersList,
-    })
-
-    let data = await response.text()
-    console.log(data)
-}
-
+// Setting child container fill parent container's width and height
+// https://dev.to/hmintoh/setting-child-container-fill-parent-container-s-width-and-height-1gdf
 function App() {
-    // return (
-    //     <div class="wrapper">
-    //         <nav>navbar</nav>
-    //         <main>body</main>
-    //         <footer>footer</footer>
-    //     </div>
-    // )
     return (
-        // <PacketContext.Provider value={{ str: "Hello, world" }}>
-        <div className="App ">
-            <MyNavbar />
-            <div className="row">
-                <div className="column">Some text</div>
-                <div className="column">
-                    {/* Some text2 */}
-                    <MyTable
-                        packets={Array(30)
-                            .fill(null)
-                            .map((element, index) => "string " + index)}
-                    ></MyTable>
-                </div>
-            </div>
-            {/* <main>body</main> */}
-
-            {/*<Container fluid full-height>
-                <Row className="w-100 h-100">
-                    <Col>
-                        <Placeholder className="w-100 h-100" />
-                    </Col>
-                    <Col>
-                        <Placeholder className="w-100 h-100" />
-                    </Col>
-                </Row>
-            </Container> */}
-            {/* <Container fluid className="  w-100 h-100">
-                <Row className="w-100 h-100">
-                    <Col className="w-100 h-100">
-                        <MyTable
-                            packets={Array(30)
-                                .fill(null)
-                                .map((element, index) => "string " + index)}
-                        ></MyTable>
-                    </Col>
-                    <Col>
-                        <Placeholder className="w-100 h-100" />
-                    </Col>
-                </Row>
-            </Container> */}
+        <div id="wrapper">
+            {/* <MyNavbar /> */}
+            <div id="nav">Navbar</div>
+            <MyTable
+                packets={Array(30)
+                    .fill(null)
+                    .map((element, index) => "string " + index)}
+            ></MyTable>
+            <div id="footer">footer</div>
         </div>
-        // </PacketContext.Provider>
     )
 }
 
--- src/MyTable.js
+++ src/MyTable.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react"
 import Table from "react-bootstrap/Table"
-import "./App.css"
+import "./MyTable.css"
 import { PacketContext, LoadPacket } from "./PacketContext"
 
 function MyRow(props) {
@@ -16,25 +16,271 @@ function MyRow(props) {
 // https://wpdatatables.com/html-table-with-fixed-header-and-scrollable-body/
 
 // Fixed header
-// https://github.com/mdbootstrap/bootstrap-table-fixed-header
+//
 export default function MyTable(props) {
     const value = useContext(PacketContext)
     console.log(props)
     console.log(value)
     return (
-        // <Table striped hover responsive="sm">
-        <Table striped>
-            <thead>
+        <table className="fixed-header">
+            <thead className="header">
                 <tr>
-                    <th>Date, time (UTC)</th>
-                    <th>Coordinates, RAW packet</th>
+                    <th>head 1</th>
+                    <th>head 2</th>
+                    <th>head 3</th>
                 </tr>
             </thead>
-            <tbody>
-                {props.packets.map((element, index) => {
-                    return <MyRow key={index} dateTime="2354" packet={element.toString()} />
-                })}
+            <tbody className="scrollable">
+                <tr>
+                    <td>body 1</td>
+                    <td>body 2</td>
+                    <td>body 3</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
+                <tr>
+                    <td>body 11</td>
+                    <td>body 22</td>
+                    <td>body 33</td>
+                </tr>
             </tbody>
-        </Table>
+        </table>
+
+        // <Table striped>
+        //     <thead>
+        //         <tr>
+        //             <th>Date, time (UTC)</th>
+        //             <th>Coordinates, RAW packet</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         {props.packets.map((element, index) => {
+        //             return <MyRow key={index} dateTime="2354" packet={element.toString()} />
+        //         })}
+        //     </tbody>
+        // </Table>
+
+        // <Table striped hover responsive="sm">
+        // <Table striped>
+        //     <thead>
+        //         <tr>
+        //             <th>Date, time (UTC)</th>
+        //             <th>Coordinates, RAW packet</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         {props.packets.map((element, index) => {
+        //             return <MyRow key={index} dateTime="2354" packet={element.toString()} />
+        //         })}
+        //     </tbody>
+        // </Table>
     )
 }
